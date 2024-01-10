388d7cee992e913ac3921d57b0fcd7f4086f44fd
HideSSH <102421846+hidessh99@users.noreply.github.com>
2024-01-10 19:37:48 +0700

    Update installer3.sh

--- Marzban/installer3.sh
+++ Marzban/installer3.sh
@@ -16,3 +16,17 @@ echo $response
 token=$(echo $response | jq -r '.access_token')
 echo $token > token.txt
 
+#add on Vmess
+wget -O /usr/local/bin/add-vmess-user "https://raw.githubusercontent.com/hidessh99/Developers/main/Marzban/gen-vmess.sh"
+wget -O /usr/local/bin/del-vmess-user "https://raw.githubusercontent.com/hidessh99/Developers/main/Marzban/del-vmess.sh"
+
+#add on Trojan
+wget -O /usr/local/bin/add-trojan-user "https://raw.githubusercontent.com/hidessh99/Developers/main/Marzban/gen-trojan.sh"
+wget -O /usr/local/bin/del-trojan-user "https://raw.githubusercontent.com/hidessh99/Developers/main/Marzban/del-vmess.sh"
+
+#permission
+chmod +x /usr/local/bin/add-vmess-user
+chmod +x /usr/local/bin/del-vmess-user
+chmod +x /usr/local/bin/add-trojan-user
+chmod +x /usr/local/bin/del-trojan-user
+
