3795dba8253c5be0de0f7d8664000d25e7ff176a
HideSSH <102421846+hidessh99@users.noreply.github.com>
2024-02-16 02:14:43 +0700

    Update add-noobzvpn.sh

--- add-noobzvpn.sh
+++ add-noobzvpn.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
+IP=$(wget -qO- ifconfig.me/ip);
+#bisa custom domain sendiri
+domain=$(cat /etc/v2ray/domain)
 
+read -p "Username : " Login
+read -p "Password : " Pass
+
+#custom masa aktif akun vpn 30 day
+masaaktif=30 
+#noobz perintah
+noobzvpns --add-user $Login $Pass
+noobzvpns --expired-user $masaaktif
+clear
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /etc/log-create-user.log
+echo -e "         Noobzvpn Account Account      " | tee -a /etc/log-create-user.log
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /etc/log-create-user.log
+echo -e "Username       : $Login "
+echo -e "Password       : $Pass"
+echo -e "Remarks : $Login" | tee -a /etc/log-create-user.log
+echo -e "Domain : $domain" | tee -a /etc/log-create-user.log
+echo -e "==============================="
