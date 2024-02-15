da3102dfeb7587fb748ac153f46bc337ede565bc
HideSSH <102421846+hidessh99@users.noreply.github.com>
2024-02-16 02:16:19 +0700

    Update add-noobzvpn.sh

--- add-noobzvpn.sh
+++ add-noobzvpn.sh
@@ -11,6 +11,14 @@ masaaktif=30
 #noobz perintah
 noobzvpns --add-user $Login $Pass
 noobzvpns --expired-user $masaaktif
+
+#perintah akun ssh
+useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
+expi="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
+echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
+hariini=`date -d "0 days" +"%Y-%m-%d"`
+expi=`date -d "$masaaktif days" +"%Y-%m-%d"`
+
 clear
 echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /etc/log-create-user.log
 echo -e "         Noobzvpn Account Account      " | tee -a /etc/log-create-user.log
