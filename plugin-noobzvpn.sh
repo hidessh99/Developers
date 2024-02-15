e806dd4647381128e80ba2e1cbcdf833b0cb00d1
HideSSH <102421846+hidessh99@users.noreply.github.com>
2024-02-16 02:18:27 +0700

    Update plugin-noobzvpn.sh

--- plugin-noobzvpn.sh
+++ plugin-noobzvpn.sh
@@ -3,8 +3,8 @@
 #noobz dan akun ssh
 
 #add on SSH
-wget -O /usr/local/bin/add-ssh-user "https://raw.githubusercontent.com/hidessh99/Developers/main/add-ssh.sh"
-wget -O /usr/local/bin/del-ssh-user "https://raw.githubusercontent.com/hidessh99/Developers/main/del-ssh.sh"
+wget -O /usr/local/bin/add-ssh-user "https://raw.githubusercontent.com/hidessh99/Developers/main/add-noobzvpn.sh"
+wget -O /usr/local/bin/del-ssh-user "https://raw.githubusercontent.com/hidessh99/Developers/main/del-noobzvpn.sh"
 
 #permission
 chmod +x /usr/local/bin/add-ssh-user
